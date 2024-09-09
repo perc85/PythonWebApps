ec0e492b95f43e754f63e7a34412ee767f8a0e47
Carlos <perc8517@bears.unco.edu>
2024-09-09 14:44:02 -0600

    Started Proj 2

---
+++ Superhero/config/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from hero.views import HulkView
+
+urlpatterns = [
+    path('',        HulkView.as_view()),
+]
---
+++ Superhero/hero/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+
+class HulkView(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Hulk',
+            'id': 'Bruce Banner',
+            'image': '/static/images/hulk.jpg'
+        }
