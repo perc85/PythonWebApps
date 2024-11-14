628d4c8650d9e37acc2f769b9d113cebe4911aa2
Carlos Perches <perc8517@bears.unco.edu>
2024-11-13 20:23:49 -0700

    Added project 8

---
+++ Student/week9/Superhero_articles/articles/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('', views.article_list, name='article_list'),
+    path('article/<int:pk>/', views.article_detail, name='article_detail'),
+    path('article/new/', views.article_create, name='article_create'),
+    path('article/<int:pk>/edit/', views.article_edit, name='article_edit'),
+    path('article/<int:pk>/delete/', views.article_delete, name='article_delete'),
+]
